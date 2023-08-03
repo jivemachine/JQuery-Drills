85c9119a1147dc36e65684f76e48c27f0a81c911
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-03 12:28:42 -0500

    finished 2nd & 3rd tasks

--- manipulating-HTML-Drill/DOM/app.js
+++ manipulating-HTML-Drill/DOM/app.js
@@ -11,3 +11,52 @@ let btnTxt = document.createTextNode('Submit');
 btn.append(btnTxt);
 
 document.body.append(btn);
+
+// num 2nd task
+let two = document.createElement('h3');
+let twoTxt = document.createTextNode('# 2');
+two.append(twoTxt);
+document.body.append(two);
+
+// second task
+let textBox = document.createElement('input');
+let textBoxButton = document.createElement('button');
+let textBoxButtonText = document.createTextNode("Submit");
+textBoxButton.append(textBoxButtonText);
+textBoxButton.className = "2Btn";
+textBox.className = "2Input";
+
+document.body.append(textBox);
+document.body.append(textBoxButton);
+
+document.addEventListener('click', function(e) {
+    if(e.target.className === "2Btn" && textBox.value != '') {
+        alert(textBox.value);
+    }
+});
+
+// numbering third task
+let three = document.createElement('h3');
+let threeTxt = document.createTextNode('# 3');
+three.append(threeTxt);
+
+document.body.append(three);
+
+// task three
+let div = document.createElement('div');
+div.style.height = "50px";
+div.style.width = "50px";
+div.style.backgroundColor = "black";
+document.body.append(div);
+
+document.addEventListener('mouseover', function(e) {
+    if(e.target.tagName === "DIV") {
+        e.target.style.backgroundColor = "goldenrod";
+    }
+});
+
+document.addEventListener('mouseout', function(e) {
+    if(e.target.tagName === "DIV") {
+        e.target.style.backgroundColor = "black";
+    }
+});
