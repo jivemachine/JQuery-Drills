673820108cff158e5f2433bb91b2fccedfd0bc88
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-03 12:06:14 -0500

    finished first task: add btn

---
+++ manipulating-HTML-Drill/DOM/app.js
@@ -0,0 +1,13 @@
+// numbering first task
+let one = document.createElement('h3');
+let oneTxt = document.createTextNode('# 1');
+one.append(oneTxt);
+
+document.body.append(one);
+
+// first task
+let btn = document.createElement('button');
+let btnTxt = document.createTextNode('Submit');
+btn.append(btnTxt);
+
+document.body.append(btn);
