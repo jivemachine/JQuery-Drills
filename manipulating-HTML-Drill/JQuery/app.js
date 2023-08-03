c91ad8afd037e93290e1419de9593aee7320b78b
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-03 13:35:17 -0500

    finished task 2

--- manipulating-HTML-Drill/JQuery/app.js
+++ manipulating-HTML-Drill/JQuery/app.js
@@ -14,3 +14,16 @@ $('.btn1').on('click', function(e) {
 
 // numbering second task
 $('body').append('<h3># 2</h3>');
+
+// task two
+let $button = "<button class='btn2'>submit</button>";
+let $textBox = '<input type="textbox" class="input2">';
+$('body').append($textBox);
+$('body').append($button);
+
+$('.btn2').on('click', alertF);
+
+function alertF() {
+    let element = $('.input2');
+    alert(element.val());
+}
