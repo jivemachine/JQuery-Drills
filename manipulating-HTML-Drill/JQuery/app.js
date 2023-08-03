334114f9f5cf0ad156f7ae729ae7715b4da669e8
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-03 13:26:06 -0500

    finished task 1

--- manipulating-HTML-Drill/JQuery/app.js
+++ manipulating-HTML-Drill/JQuery/app.js
@@ -1,2 +1,13 @@
 // numbering first task
 $('body').append('<h3># 1</h1>');
+
+// first task
+let button = "<button>Submit</button>";
+$('body').append(button);
+$('button').attr('class', 'btn1');
+
+
+
+$('.btn1').on('click', function(e) {
+    alert("hi friend :)");
+});
